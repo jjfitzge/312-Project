413f3d62ae36620009467aaf57cd475e86ba39be
Nick <naminor@buffalo.edu>
2022-04-06 02:10:37 -0400

    Added home page and changed file structure to allow for static files #4

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder=t_dir)
 @app.route('/', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template('loginpage.html')
+        return render_template('index.html')
 @app.route('/mainpage')
 def return_news():
    return render_template('mainpage.html')
