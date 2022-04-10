794d446d98366b46bdaf472ae2393962f22b3d71
Kchan42 <Kchan42@buffalo.edu>
2022-04-10 14:02:40 -0400

    added register.html redirect to register

--- src/app.py
+++ src/app.py
@@ -4,13 +4,22 @@ import os
 t_dir = os.path.abspath('../html')
 app = Flask(__name__, template_folder=t_dir)
 
+
 @app.route('/', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template('index.html')
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+
+
 @app.route('/mainpage')
 def return_news():
-   return render_template('mainpage.html')
+    return render_template('mainpage.html')
 
 
 if __name__ == '__main__':
