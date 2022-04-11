9bf4e8a5adcd01e4098ef1bb26a1dff94238e908
James <jjfitzge@buffalo.edu>
2022-04-11 12:00:49 -0400

    changed example func in db

--- src/utility/database.py
+++ src/utility/database.py
@@ -12,7 +12,6 @@ def create_entry(collection, email, username, password):
     # In the full app password can't be stored directly
     data = {"email": email, "username": username, "password": password}
     collection.insert_one(data)
-    return collection.find({"id": id}, {"_id": 0, "id": 1, "email": 1, "username": 1})
 
 
 def list_entry(collection):
