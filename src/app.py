d35d86dcc27ec2b2de0f3129fdf92cd262ca60c6
James <jjfitzge@buffalo.edu>
2022-04-11 11:41:23 -0400

    reorganized files and set up docker env

--- src/app.py
+++ src/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
 import os
 
-t_dir = os.path.abspath('../html')
-app = Flask(__name__, template_folder=t_dir)
+#t_dir = os.path.abspath('./html')
+app = Flask(__name__, template_folder='./html')
+
 
 @app.route('/', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template('index.html')
+
+
 @app.route('/mainpage')
 def return_news():
-   return render_template('mainpage.html')
+    return render_template('mainpage.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    HOST, PORT = "0.0.0.0", 8000
+    app.run(HOST, PORT)
