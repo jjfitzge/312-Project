da15e6b68381c60a507d01c3fd36b11a92fd550c
James <jjfitzge@buffalo.edu>
2022-04-19 11:06:32 -0400

    websocket implemented and register

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
-import os
-import html
 from flask_socketio import SocketIO, emit, send
-import json
+
 
 #t_dir = os.path.abspath('./html')
 app = Flask(__name__, template_folder='./html')
@@ -20,9 +18,28 @@ def return_news():
     return render_template('mainpage.html')
 
 
-def check_str_for_safety(string):
-    retstr = html.escape(string)
-    return retstr
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+    if request.method == "POST":
+        jsonData = request.form
+        # username -> "user"
+        # password -> "pass"
+        # password2 -> "pass2"
+        print(jsonData)
+        user = jsonData
+        username = jsonData.get('user')
+        password = jsonData.get('pass')
+        pass2 = jsonData.get('pass2')
+        print(username, password, pass2)
+        # if the passwords are equal then let them create the account and sign in
+        if password == pass2:
+            pass
+        else:
+            return render_template('register.html')
+        # if not then, take them back to the page to try and register again
+        # try divs that are hidden
 
 
 @socketio.on('connect')
