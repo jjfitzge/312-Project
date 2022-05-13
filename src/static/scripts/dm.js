ffc9845c064c496a2a461a3fab83857a928a220a
Nick <naminor@buffalo.edu>
2022-05-12 23:59:28 -0400

    Added areas for open dms and message text to be displayed #19

--- src/static/scripts/dm.js
+++ src/static/scripts/dm.js
@@ -1,5 +1,7 @@
 const socket = new WebSocket('ws://' + window.location.host + '/websocket');
 
+const messageData = [];
+
 // Called whenever data is received from the server over the WebSocket connection
 socket.onmessage = function (ws_message) {
     const message = JSON.parse(ws_message.data);
