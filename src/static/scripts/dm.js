7927faeb018e13c93ac14b3648e765c6649e7147
Nick <naminor@buffalo.edu>
2022-05-13 21:27:11 -0400

    Added functionality for showing displayed DMs over websockets #20

--- src/static/scripts/dm.js
+++ src/static/scripts/dm.js
@@ -7,10 +7,11 @@ let recipient;
 socket.onmessage = function (ws_message) {
     const message = JSON.parse(ws_message.data);
     const messageType = message.messageType
+    console.dir(`Received websocket message of type: ${messageType}\n${message}\n\n${JSON.stringify(message)}`)
 
     switch (messageType) {
         case 'directMessage':
-            addMessage(message);
+            addWebsocketMessage(message);
             break;
         default:
             console.log(`received an invalid WS messageType: ${messageType}`);
@@ -20,6 +21,9 @@ socket.onmessage = function (ws_message) {
 function initDMPage() {
     initRecipient();
     getOpenDMs();
+    // if(messageData)
+    //     showMessages(messageData);
+    // else console.log("No message data yet...");
 }
 
 function getOpenDMs() {
@@ -30,11 +34,12 @@ function getOpenDMs() {
             const dms = JSON.parse(this.response);
             messageData = dms;
             console.log(`Message Data ${messageData}`);
-            
+
             
             for (let dm of dms.openDMs) {
                 addOpenDM({username: dm.otherUsersName});
             }
+            showMessages(messageData, recipient);
             // for (let user of users) {
             //     console.log(`Adding User ${user.username}`);
             //     addOnlineUser(user);
@@ -51,11 +56,11 @@ function initRecipient() {
         if (this.readyState === 4 && this.status === 200) {
             console.log(`response: ${this.response}`);
             // const data = JSON.parse(this.response);
-            // setOpenMessage(data['toUser']);
+            // setRecipient(data['toUser']);
             const data = this.response;
             console.log(`Datatype: ${(typeof data)}`)
             const toUser = data.slice(1, data.length-1); // Remove quotations from the data
-            setOpenMessage(toUser);
+            recipient = toUser;
 
 
             console.log("set user to ", toUser);
@@ -79,13 +84,21 @@ function sendDM() {
     }
 }
 
-function setOpenMessage(username) {
+function setRecipient(username) {
     console.log(`Set recipient to ${username}`);
     recipient = username;
+    // refreshMessagePane(username);
 }
 
-function changeRecipient() {
+function refreshMessagePane(newRecipient) {
+    deleteMessages();
+    showMessages(messageData, newRecipient)
+}
 
+function deleteMessages() {
+    // const messageList = 
+    const messageView = document.getElementsByClassName('messages-list')[0];
+    messageView.remove();
 }
 
 function addOpenDM(user) {
@@ -94,9 +107,69 @@ function addOpenDM(user) {
     const newDiv = document.createElement('div');
     newDiv.classList.add("openDM-list-item");
     const username = user['username'];
-    newDiv.setAttribute("onclick", "setOpenMessage(this.innerText);")
+    newDiv.setAttribute("onclick", "setRecipient(this.innerText);")
     newDiv.innerHTML = `<b>${username}</b>`;
 
     openDMList.append(newDiv);
+
+}
+
+function addWebsocketMessage(data) {
+    const username = data['username'];
+    const toUser = data['toUser'];
+    console.log(`Sent from ${username} to ${toUser}`)
+    if (username != recipient && username != toUser) return;
+
+    const message = data['comment'];
+    const color = data['color'];
+
+    const messageView = document.getElementsByClassName('chat-view')[0];
+    const newMsgDiv = document.createElement('div');
+    newMsgDiv.innerHTML += `<div class="message-container"><b>${username}:</b> ${message}</div>`;
+
+    messageView.prepend(newMsgDiv);
+}
+
+function showMessages(data, otherUser) {
+    if (!data || !otherUser) return;
+
+    const messageView = document.getElementsByClassName('chat-view')[0];
+
+    const currentUser = data['currentUser'];
+    const openDMs = data['openDMs'];
+    const messages = getParticularMessages(openDMs, otherUser);
+
+    console.log(`=== Data ===\ncurrentUser: ${currentUser}\nopenDMs:${openDMs}`);
+
+    const allMsgDiv = document.createElement('div');
+    allMsgDiv.classList.add('messages-list');
+    for (let msg of messages)
+        allMsgDiv.innerHTML += `<div class="message-container"><b>${msg.username}:</b> ${msg.comment}</div>`;
+
+    messageView.append(allMsgDiv);
+
+    console.log('printingDMs');
+    for (let msg of messages) {
+        console.log(msg);
+        console.log(msg.username, msg.comment)
+    }
+}
+
+function getParticularMessages(openDMs, otherUser) {
+    let messages;
+    console.log(`Iterating open dms ${openDMs}`)
+    for (let dm of openDMs) {
+        if (dm.otherUsersName === otherUser) {
+            messages = dm.messages;
+        }
+    }
+    console.log(`Returning messages: ${JSON.stringify(messages)}`);
+    return messages;
+}
+
+function createMessage(msg) {
+    const newMsg = document.createElement('div');
+    newdiv.classList.add('message-container');
+    // const username = 
 
 }
